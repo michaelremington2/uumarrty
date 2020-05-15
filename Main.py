6ed6c384db37e7a99911ff419043db8259aba2e3
michaelremington2 <53287026+michaelremington2@users.noreply.github.com>
2020-05-14 21:26:57 -0700

    Update Main.py
    integrated organism energy settings and alive/ dead dynamic organism counters.

--- Main.py
+++ Main.py
@@ -1,6 +1,9 @@
 import pygame
 import random
 import Organisms
+import time
+#import pandas as pd
+#from collections import Counter
 
 class sim:
     def __init__(self,initial_snake_pop,initial_kr_pop,initial_bush_pop,initial_grass_pop):
@@ -18,28 +21,17 @@ class sim:
         self.game_display = pygame.display.set_mode((self.width,self.height))
         self.caption = pygame.display.set_caption(self.title)
         self.font = pygame.font.SysFont(None, 25)
+        self.dead_kr = 0
+        self.dead_snake = 0 
+        self.dead_grass = 0 
+        self.dead_bush = 0
+        self.kr_energy = 0
+        self.snake_energy = 0
 
     def background(self):
         'Generates Game Background. This should be run first.'
         self.game_display.fill(self.background_color)
 
-    def add_organisms(self,org_list,org_type,time_counter):
-        '''This function designates shape and color of objects depending on the organism type.
-        org type is either KRAT, SNAKE, BUSH, GRASS.'''
-        if org_type == 'KRAT':   
-            for org in org_list:
-                pygame.draw.circle(self.game_display,org.color,[org.x,org.y],org.size)
-                org.krat_move()
-        elif org_type == 'SNAKE':
-            for org in org_list:
-                org.snake_energy(time_counter)
-                pygame.draw.circle(self.game_display,org.color,[org.x,org.y],org.size)
-                org.snake_move()
-        elif org_type in ['BUSH','GRASS']:
-            for org in org_list:
-                pygame.draw.rect(self.game_display,org.color,(org.x,org.y,org.wid,org.length))
-        pygame.display.update()
-    
     def set_organisms(self):
         self.SNAKES = dict(enumerate(
             [Organisms.snake(self.width,self.height) for i in range(self.initial_snake_pop)]
@@ -53,26 +45,104 @@ class sim:
         self.GRASSES = dict(enumerate(
             [Organisms.grass(self.width,self.height) for i in range(self.initial_grass_pop)]
             ))
+
+    def add_organisms(self,org_list,org_type,time):
+        '''This function designates shape, color, and sets energy and move counters of objects depending on the organism type.
+        org type is either KRAT, SNAKE, BUSH, GRASS.'''
+        if org_type == 'KRAT':   
+            for org in org_list:
+                org.krat_energy(time)
+                pygame.draw.circle(self.game_display,org.color,[org.x,org.y],org.size)
+                org.krat_move()
+        elif org_type == 'SNAKE':
+            for org in org_list:
+                org.snake_energy(time)
+                pygame.draw.circle(self.game_display,org.color,[org.x,org.y],org.size)
+                org.snake_move()
+                #org.move()
+        elif org_type in ['BUSH','GRASS']:
+            for org in org_list:
+                pygame.draw.rect(self.game_display,org.color,(org.x,org.y,org.wid,org.length))
+        pygame.display.update()
     
-    def time_set(self,n,time_counter,fps):
-        if (n % 13) == 0:
-            return time_counter+1
-        else:
-            return time_counter
+    def snake_attack(self):
+        for i, snake in self.SNAKES.items():
+            for j, kr in self.KANGAROO_RATS.items():
+                dx = abs(kr.x - snake.x)
+                dy = abs(kr.y - snake.y)
+                R = 5
+                if dx <= R and dy <= R:
+                    chance_to_kill = 50
+                    strike = random.randrange(0,100)
+                    if strike <= chance_to_kill:
+                        kr.alive = False
+                        kr.krat_dead()
+                        snake.energy_counter += kr.energy_counter*snake.metabolism
+                        pygame.display.update()
+
+    def krat_grass_attack(self):
+        for i, grass in self.GRASSES.items():
+            for j, kr in self.KANGAROO_RATS.items():
+                dx = abs(grass.x - kr.x)
+                dy = abs(grass.y - kr.y)
+                R = 3
+                if dx <= R and dy <= R:
+                    grass.alive = False
+                    grass.grass_dead()
+                    self.dead_grass += 1
+                    kr.energy_counter += grass.energy_counter*kr.metabolism
+                    pygame.display.update()
     
-    def population_counts_and_time(self,count,num_snakes,num_rats):
-        self.text1 = self.font.render("Time Step: "+str(count), True, (0,0,0))
+    def krat_bush_attack(self):
+        for i, bush in self.BUSHES.items():
+            for j, kr in self.KANGAROO_RATS.items():
+                dx = abs(bush.x - kr.x)
+                dy = abs(bush.y - kr.y)
+                R = 5
+                if dx <= R and dy <= R:
+                    kr.energy_counter += 10*kr.metabolism
+                    bush.energy_counter += -10
+                    bush.bush_dead()
+                    pygame.display.update()
+
+    def krat_energy_total(self,time_counter):
+        kr=self.KANGAROO_RATS.get(1)
+        print('Kangaroo Rat 1 Energy Counter ' + str(kr.energy_counter)+ 'time: ' +str(time_counter))
+
+    def krat_count(self):
+        dead_krat_list = []
+        for j, kr in self.KANGAROO_RATS.items():
+            if kr.alive == False:
+                dead_krat_list.append(j)
+        self.dead_kr=len(set(dead_krat_list))
+
+    def snake_count(self):
+        dead_snake_list = []
+        for j, snake in self.SNAKES.items():
+            if snake.alive == False:
+                dead_snake_list.append(j)
+        self.dead_snake=len(set(dead_snake_list))
+    
+    def stopwatch(self,seconds):
+        start = round(time.time())
+        elapsed = 0
+        while elapsed < seconds:
+            elapsed = round(time.time()) - start
+    
+    def population_counts_and_time(self,time,num_snakes,num_rats):
+        '''Sets legend. counts for time, snake population, and krat population'''
+        self.text1 = self.font.render("Time Step: "+str(time), True, (0,0,0))
         self.game_display.blit(self.text1,(0,0))
-        self.text2 = self.font.render("Snakes: "+ str(len(num_snakes)), True, (0,0,0))
+        self.text2 = self.font.render("Snakes: "+ str(len(num_snakes)-self.dead_snake), True, (0,0,0))
         self.game_display.blit(self.text2,(0,20))
-        self.text3 = self.font.render("Kangaroo Rats: "+ str(len(num_rats)), True, (0,0,0))
+        self.text3 = self.font.render("Kangaroo Rats: "+ str(len(num_rats) - self.dead_kr), True, (0,0,0))
         self.game_display.blit(self.text3,(0,40))
 
     def main(self):
         '''Main function that runs and compiles the program'''
-        time_counter=0
-        n = 0
         fps = 30
+        start = round(time.time())
+        time_counter = 0
         self.set_organisms()
         while True:
             for event in pygame.event.get():
@@ -81,21 +151,22 @@ class sim:
                     quit()
             self.background()
             self.population_counts_and_time(time_counter,self.SNAKES,self.KANGAROO_RATS)
-            self.add_organisms(self.SNAKES.values(),'SNAKE',n)
-            self.add_organisms(self.KANGAROO_RATS.values(),'KRAT',n)
-            self.add_organisms(self.BUSHES.values(),'BUSH',n)
-            self.add_organisms(self.GRASSES.values(),'GRASS',n)
+            self.add_organisms(self.SNAKES.values(),'SNAKE',time_counter)
+            self.add_organisms(self.KANGAROO_RATS.values(),'KRAT',time_counter)
+            self.add_organisms(self.BUSHES.values(),'BUSH',time_counter)
+            self.add_organisms(self.GRASSES.values(),'GRASS',time_counter)
+            self.snake_attack()
+            self.krat_grass_attack()
+            self.krat_bush_attack()
+            #self.krat_energy_total(time_counter)
             self.clock.tick(fps)
-            n=n+1
-            time_counter = self.time_set(n,time_counter,fps)
-            #print(time_counter)
+            self.krat_count()
+            time_counter = round(time.time()) - start
     
 if __name__ ==  "__main__":
     initial_snake_pop = 5
-    initial_kr_pop = 20
-    initial_bush_pop = 3
-    initial_grass_pop = 400
+    initial_kr_pop = 30
+    initial_bush_pop = 2
+    initial_grass_pop = 100
     sim = sim(initial_snake_pop,initial_kr_pop,initial_bush_pop,initial_grass_pop)
     sim.main()
-    
-    

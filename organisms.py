7df68eb9420c90c51c6e84e2edaf9e00cbcf869f
michaelremington2 <53287026+michaelremington2@users.noreply.github.com>
2020-08-07 11:17:42 -0700

    Update organisms.py
    Updated movement functions.

--- organisms.py
+++ organisms.py
@@ -71,75 +71,69 @@ class snake(organism):
         self.size = 5
         self.boundary_x = boundary_x
         self.boundary_y = boundary_y
-        self.energy_counter = random.randrange(750,950)
+        self.energy_counter = random.randrange(150,200)
         self.alive = True
         self.metabolism = 0.5 #ability to metabolize food
-
-    def snake_walk_x(self):
-        #### Old 
-        long_distiance = random.randrange(0,100)
-        if long_distiance <=95:
-            movement = random.randrange(-2,3)
-        else:
-            movement = random.randrange(-24,25)*2
-        return movement
-
-    def snake_walk_y(self):
-        #### Old 
-        long_distiance = random.randrange(0,100)
-        if long_distiance <=95:
-            movement = random.randrange(-2,3)
-        else:
-            movement = random.randrange(-24,25)*2
-        return movement
 
     def snake_move_distance(self):
         y_short_distance = random.randrange(1,4)
         x_short_distance = random.randrange(1,4)
         y_long_distance = random.randrange(5,15)
         x_long_distance = random.randrange(5,15)
-        long_move_coefficent = random.randrange(0,100)
+        move_coefficent = random.randrange(0,100)
         if self.energy_counter <=100:
-            self.chance_to_move_long = 50
+            self.chance_to_move_long = range(0,90)
+            self.chance_to_move_short = range(90,95)
+            self.chance_to_not_move = range(95,100)
         else:
-            self.chance_to_move_long = 10 
-        if long_move_coefficent <= self.chance_to_move_long:
+            self.chance_to_move_long = range(0,25)
+            self.chance_to_move_short = range(25,65)
+            self.chance_to_not_move = range(65,100)
+        if  move_coefficent in self.chance_to_move_long:
             self.y_distance = y_long_distance
             self.x_distance = x_long_distance 
-        else:
+        elif move_coefficent in self.chance_to_move_short:
             self.y_distance = y_short_distance
-            self.x_distance = x_short_distance 
+            self.x_distance = x_short_distance
+        elif move_coefficent in self.chance_to_not_move:
+            self.y_distance = 0
+            self.x_distance = 0 
 
     def snake_move_direction(self):
         self.y_direction = random.choice([0,1])
         self.x_direction = random.choice([0,1])
         self.snake_move_distance()
-        if self.y_direction == 1:
-            # up 
-            for i in range(self.y_distance):
-                self.move_y = 1
-        elif self.y_direction == 0:
-            # down 
-            for i in range(self.y_distance):
-                self.move_y = -1
-        if self.x_direction == 1:
-            # right 
-            for i in range(self.x_distance):
-                self.move_x = 1
-        elif self.x_direction == 0:
-            for i in range(self.x_distance):
-                self.move_x = -1
+        if self.x_distance == 0 and self.y_distance == 0:
+            time_in_loop = random.randrange(5000,10000)
+            for i in range(time_in_loop):
+                self.move_x = 0
+                self.move_y = 0
+                self.x += self.move_x
+                self.y += self.move_y
+        else:    
+            if self.y_direction == 1:
+                # up 
+                for i in range(self.y_distance):
+                    self.move_y = 1
+                    self.y += self.move_y
+            elif self.y_direction == 0:
+                # down 
+                for i in range(self.y_distance):
+                    self.move_y = -1
+                    self.y += self.move_y
+            if self.x_direction == 1:
+                # right 
+                for i in range(self.x_distance):
+                    self.move_x = 1
+                    self.x += self.move_x
+            elif self.x_direction == 0:
+                for i in range(self.x_distance):
+                    self.move_x = -1
+                    self.x += self.move_x
 
     def snake_move(self):
         if self.alive == True:
-            chance_to_move = random.randrange(0,100)
-            if chance_to_move <= 50:
-                self.snake_move_direction()
-            else:
-                self.move_x = 0
-                self.move_y = 0
-            self.x += self.move_x
-            self.y += self.move_y
+            self.snake_move_direction()
             #print( 'snake x:' + str(self.x) + ' y:' + str(self.y))
             self.boundary()
 
@@ -154,9 +148,9 @@ class snake(organism):
 
     def snake_energy(self,time):
         if self.alive == True:
-            if time % 15 == 0:
-                self.energy_counter = self.energy_counter - 1
-            elif time % 15 != 0:
+            if time % 10 == 0:
+                self.energy_counter = self.energy_counter - 2
+            else:
                 self.energy_counter = round(self.energy_counter)
             if self.energy_counter <= 1:
                 self.alive = False
@@ -173,76 +167,69 @@ class kangaroo_rat(organism):
         self.size = 3
         self.boundary_x = boundary_x
         self.boundary_y = boundary_y
-        self.energy_counter = random.randrange(190,220)
+        self.energy_counter = random.randrange(50,60)
         self.alive = True
         self.metabolism = .8
     
-    def krat_walk_x(self):
-        #old
-        long_distiance = random.randrange(0,100)
-        if long_distiance <=80:
-            movement = random.randrange(-2,3)
-        else:
-            movement = random.randrange(-6,7)*2
-        return movement 
-
-    def krat_walk_y(self):
-        #old
-        long_distiance = random.randrange(0,100)
-        if long_distiance <=80:
-            movement = random.randrange(-2,3)
-        else:
-            movement = random.randrange(-6,7)*2
-        return movement
-
     def krat_move_distance(self):
         y_short_distance = random.randrange(1,4)
         x_short_distance = random.randrange(1,4)
-        y_long_distance = random.randrange(5,15)
-        x_long_distance = random.randrange(5,15)
-        long_move_coefficent = random.randrange(0,100)
-        if self.energy_counter <=40:
-            self.chance_to_move_long = 80
+        y_long_distance = random.randrange(5,10)
+        x_long_distance = random.randrange(5,10)
+        move_coefficent = random.randrange(0,100)
+        if self.energy_counter <=20:
+            self.chance_to_move_long = range(0,90)
+            self.chance_to_move_short = range(90,95)
+            self.chance_to_not_move = range(95,100)
         else:
-            self.chance_to_move_long = 10 
-        if long_move_coefficent <= self.chance_to_move_long:
+            self.chance_to_move_long = range(0,10)
+            self.chance_to_move_short = range(10,75)
+            self.chance_to_not_move = range(75,100)
+        if  move_coefficent in self.chance_to_move_long:
             self.y_distance = y_long_distance
             self.x_distance = x_long_distance 
-        else:
+        elif move_coefficent in self.chance_to_move_short:
             self.y_distance = y_short_distance
-            self.x_distance = x_short_distance 
+            self.x_distance = x_short_distance
+        elif move_coefficent in self.chance_to_not_move:
+            self.y_distance = 0
+            self.x_distance = 0 
 
     def krat_move_direction(self):
         self.y_direction = random.choice([0,1])
         self.x_direction = random.choice([0,1])
         self.krat_move_distance()
-        if self.y_direction == 1:
-            # up 
-            for i in range(self.y_distance):
-                self.move_y = 1
-        elif self.y_direction == 0:
-            # down 
-            for i in range(self.y_distance):
-                self.move_y = -1
-        if self.x_direction == 1:
-            # right 
-            for i in range(self.x_distance):
-                self.move_x = 1
-        elif self.x_direction == 0:
-            for i in range(self.x_distance):
-                self.move_x = -1
+        if self.x_distance == 0 and self.y_distance == 0:
+            time_in_loop = random.randrange(5000,10000)
+            for i in range(time_in_loop):
+                self.move_x = 0
+                self.move_y = 0
+                self.x += self.move_x
+                self.y += self.move_y
+        else:    
+            if self.y_direction == 1:
+                # up 
+                for i in range(self.y_distance):
+                    self.move_y = 1
+                    self.y += self.move_y
+            elif self.y_direction == 0:
+                # down 
+                for i in range(self.y_distance):
+                    self.move_y = -1
+                    self.y += self.move_y
+            if self.x_direction == 1:
+                # right 
+                for i in range(self.x_distance):
+                    self.move_x = 1
+                    self.x += self.move_x
+            elif self.x_direction == 0:
+                for i in range(self.x_distance):
+                    self.move_x = -1
+                    self.x += self.move_x
 
     def krat_move(self):
         if self.alive == True:
-            chance_to_move = random.randrange(0,100)
-            if chance_to_move <= 80:
-                self.krat_move_direction()
-            else:
-                self.move_x = 0
-                self.move_y = 0
-            self.x += self.move_x
-            self.y += self.move_y
-            #print( 'snake x:' + str(self.x) + ' y:' + str(self.y))
+            self.krat_move_direction()
             self.boundary()
 
     def krat_dead(self):
@@ -250,8 +237,6 @@ class kangaroo_rat(organism):
             self.color = (255,255,255)
             self.move_x = 0
             self.move_y = 0
-            #self.x = 0 
-            #self.y = 0 
             self.size = 0
             self.energy_counter = 0
 
@@ -311,4 +296,5 @@ class grass:
             self.size = 1
             self.energy_counter = 0
 if __name__ == "__main__":
-   pass
+    pass
+

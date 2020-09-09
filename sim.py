4c9351e8a796b6a2ac10320eb2ae490aa0f8bafc
michaelremington2 <michaelremington2@gmail.com>
2020-09-09 12:38:42 -0700

    Changed sim.py to new object names.

--- sim.py
+++ sim.py
@@ -1,13 +1,9 @@
-class microclimate:
-	def __init__(self,micro_id,width,height, top_left_corner_point = (0,0)):
-		self.micro_id = micro_id
-		self.width = width
-		self.height = hieght
-		self.start_point = top_left_corner_point
-		self.start_x = self.start_point[0]
-		self.start_y = self.start_point[1]
-		self.microclimate_type =  random.choice(['open','bush'])
-		self.micro_grid = []
+class Cell(object):
+	def __init__(self):
+		pass
+
+	
+
 
 
 	def micro_climate_grid_gen(self):
@@ -16,9 +12,10 @@ class microclimate:
             for y in range(self.start_y,self.start_y+self.micro_height):
                 point = [x,y]
                 self.micro_grid.append(point)
-class climate:
+
+class Landscape(object):
 	pass
 
-class.sim:
+class.Sim(object):
 	pass
 

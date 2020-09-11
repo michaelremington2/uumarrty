ec7c04a20f3f54e66b9264b799bf84a152cbc8bd
michaelremington2 <michaelremington2@gmail.com>
2020-09-10 18:54:03 -0700

    Added code to functions such as add_species and select species

--- sim.py
+++ sim.py
@@ -1,54 +1,55 @@
-import random
+#rng = random.Random()
+#rng = random.Random(random_seed)
 class Cell(object):
-	def __init__(self,landscape, habitat_type,rat_energy_cost,snake_energy_cost,rat_energy_gain,strike_success_probability):
+	def __init__(self,landscape, habitat_type,krat_energy_cost,snake_energy_cost,krat_energy_gain,strike_success_probability):
 		# Order: open, bush
 		self.landscape = landscape
 		self.sim_time = landscape.sim_time
-		self.rats = []
+		self.krats = []
 		self.snakes = []
 		self.habitat_type = habitat_type
-		self.rat_energy_cost = rat_energy_cost
+		self.krat_energy_cost = krat_energy_cost
 		self.snake_energy_cost = snake_energy_cost
-		self.rat_energy_gain = rat_energy_gain
+		self.krat_energy_gain = krat_energy_gain
 		self.rng = landscape.rng
 
-	def add_rat(self, rat):
-		# Add a rat to the population of this cells rats
-		pass
+	def add_krat(self, krat):
+		# Add a krat to the population of this cells krats
+		self.krats.append(krat)
 
 	def add_snake(self, snake):
-		# Add a rat to the population of this cells snakes
-		pass
+		# Add a krat to the population of this cells snakes
+		self.snakes.append(snake)
 
-	def select_rat(self):
-		#returns a rat object from this cells population of rat
-		pass
+	def select_krat(self,krat_id):
+		#returns a krat object from this cells population of krat
+		return self.krats[krat_id]
 
-	def select_snake(self):
-		#returns a snake object from this cells population of rat
-		pass
+	def select_snake(self,snake_id):
+		#returns a snake object from this cells population of snakes
+		return self.snakes[snake_id]
 
-	def pop_rat(self):
-		# Selects a rat at random from population and removes it and return it
-		rat_index = random.randint(0,len(self.rats)-1)
-		return self.rats.pop(rat_index)
+	def pop_krat(self):
+		# Selects a krat at random from population and removes it and return it
+		krat_index = self.rng.randint(0,len(self.krats))
+		return self.krats.pop(krat_index)
 
 	def pop_snake(self):
 		# Selects a snake at random from population and removes it and return it
-		snake_index = self.rng.randint(0,len(self.snakes)-1)
+		snake_index = self.rng.randint(0,len(self.snakes))
 		return self.snakes.pop(snake_index)
 
 	def predation_cycle_snake(self):
 		for snake in self.snakes:
 			snake.expend_energy(self.snake_energy_cost)
 			if self.rng < self.strike_success_probability:
-				rat = self.pop_rat() 
+				krat = self.pop_rat() 
 				snake.consume(rat)
 
 	def foraging_rat(self):
-		for rat in self.rats:
-			rat.expend_energy(self.rat_energy_cost)
-			rat.gain_energy(self.rat_energy_gain)
+		for krat in self.krats:
+			krat.expend_energy(self.krat_energy_cost)
+			krat.gain_energy(self.krat_energy_gain)
 
 
 class Landscape(object):

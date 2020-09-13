0bbc3b26da509cca75b4a5c7b8d0a920509caf09
michaelremington2 <michaelremington2@gmail.com>
2020-09-12 19:28:50 -0700

    Reorganized folder

--- ecosystem_sim/organismsim.py
+++ ecosystem_sim/organismsim.py
@@ -2,6 +2,7 @@ class Organism(object):
 	def __init__(self,landscape,energy_counter):
 		self.energy_counter = energy_counter
 		self.landscape = landscape
+		self.alive = True
 		self.rng = landscape.rng
 
 	def consume(self,energy_gain):
@@ -9,6 +10,13 @@ class Organism(object):
 
 	def get_energy_counter(self):
 		return self.energy_counter
+
+	def expend_energy(self,energy_cost):
+		self.energy_counter -= energy_cost
+
+	def natural_death(self):
+		if self.energy_counter == 0:
+			self.alive = False
 
 
 class Snake(Organism):
@@ -19,8 +27,10 @@ class Snake(Organism):
 		self.rng = landscape.rng
 
 class Krat(Organism):
-	def __init__(self,landscape,energy_counter):
+	def __init__(self,landscape,energy_counter,home_cell):
 		super().__init__(landscape,energy_counter)
 		self.energy_counter = energy_counter
 		self.landscape = landscape
+		self.home_cell = home_cell
+		self.foraging = False
 		self.rng = landscape.rng

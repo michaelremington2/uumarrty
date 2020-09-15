be88641d4b0936a6b210331b940067bc92a35588
michaelremington2 <michaelremington2@gmail.com>
2020-09-14 20:16:59 -0700

    added functions to populate cells with organisms in the landscape object.

--- ecosystem_sim/organismsim.py
+++ ecosystem_sim/organismsim.py
@@ -1,36 +1,70 @@
 class Organism(object):
-	def __init__(self,landscape,energy_counter):
-		self.energy_counter = energy_counter
-		self.landscape = landscape
-		self.alive = True
-		self.rng = landscape.rng
+    def __init__(self,energy_counter,rng):
+        self.energy_counter = energy_counter
+        self.landscape = landscape
+        self.alive = True
+        self.rng = landscape.rng
 
-	def consume(self,energy_gain):
-		self.energy_counter += energy_gain
+    def consume(self,energy_gain):
+        if energy_cost < 0:
+            RaiseValueError('gains should be positive integers')
+        self.energy_counter += energy_gain
 
-	def get_energy_counter(self):
-		return self.energy_counter
+    def get_energy_counter(self):
+        return self.energy_counter
 
-	def expend_energy(self,energy_cost):
-		self.energy_counter -= energy_cost
+    def expend_energy(self,energy_cost):
+        if energy_cost < 0:
+            RaiseValueError('Costs should be positive integers')
+        self.energy_counter -= energy_cost
 
-	def natural_death(self):
-		if self.energy_counter == 0:
-			self.alive = False
+    def natural_death(self):
+        if self.energy_counter == 0:
+            self.alive = False
+
+    def time_of_day(self,time_of_day):
+        if time_of_day >= 0 and time_of_day < 24:
+            pass
+        else:
+            RaiseValueError('Not an appropriate time of day')
 
 
 class Snake(Organism):
-	def __init__(self,landscape,energy_counter):
-		super().__init__(landscape,energy_counter)
-		self.energy_counter = energy_counter
-		self.landscape = landscape
-		self.rng = landscape.rng
+    def __init__(self,energy_counter,strike_success_probability,hunting_hours = None,rng):
+        super().__init__(landscape,energy_counter)
+        self.energy_counter = energy_counter
+        self.strike_success_probability = strike_success_probability
+        self.hunting = False
+        self.hunting_hours = self.hunting_period_gen(hunting_hours)
+        self.rng = rng
+
+    def hunting_period_gen(self,hunting_hours):
+        if hunting_hours == None:
+            hunting_hours = [0,1,2,3,4,5,20,21,22,23]
+        return hunting_hours
+
+    def hunting_period(self,time_of_day):
+        if time_of_day in hunting_hours:
+            self.hunting = True
+        else:
+            self.hunting = False
+
 
 class Krat(Organism):
-	def __init__(self,landscape,energy_counter,home_cell):
-		super().__init__(landscape,energy_counter)
-		self.energy_counter = energy_counter
-		self.landscape = landscape
-		self.home_cell = home_cell
-		self.foraging = False
-		self.rng = landscape.rng
+    def __init__(self,energy_counter,home_cell,rng):
+        super().__init__(landscape,energy_counter)
+        self.energy_counter = energy_counter
+        self.home_cell = home_cell
+        self.foraging = False
+        self.rng = rng
+
+    def hunting_period_gen(self,foraging_hours):
+        if foraging_hours == None:
+            foraging_hours = [0,1,2,3,4,5,20,21,22,23]
+        return foraging_hours
+
+    def foraging_period(self,time_of_day):
+        if time_of_day in foraging_hours:
+            self.foraging = True
+        else:
+            self.foraging = False
--- ecosystem_sim/sim.py
+++ ecosystem_sim/sim.py
@@ -5,7 +5,7 @@ from organismsim import krat
 from organismsim import snake
 
 class Cell(object):
-    def __init__(self, time_of_day, habitat_type,krat_energy_cost,snake_energy_cost,krat_energy_gain,cell_energy_pool, initial_snake_pop,initial_krat_pop,rng):
+    def __init__(self, time_of_day, habitat_type,krat_energy_cost,snake_energy_cost,krat_energy_gain,cell_energy_pool,,rng):
         #cell represents the microhabitat and governs shorter geospatial interactions
         # Order: open, bush
         self.time_of_day = time_of_day
@@ -26,14 +26,18 @@ class Cell(object):
         # Add a krat to the population of this cells snakes
         self.snakes.append(snake)
 
-    def select_krat(self):
-        #returns a krat object from this cells population of krat
-        krat_index = self.rng.randint(0,len(self.krats))
-        return krat_index
+    def select_krat(self,krat_index = None):
+        #returns a random index for the krat
+        if krat_index == None:
+            krat_index = self.rng.randint(0,len(self.krats))
+        krat = self.krats[krat_index]
+        return krat
 
-    def select_snake(self,snake_id):
+    def select_snake(self,snake_index = None):
         #returns a snake object from this cells population of snakes
-        snake_index = self.rng.randint(0,len(self.snake))
+        if snake_index == None:
+            snake_index = self.rng.randint(0,len(self.snake))
+        snake = self.snakes[snake_index]
         return snake_index
 
     def pop_krat(self,krat_index):
@@ -51,8 +55,7 @@ class Cell(object):
         for snake in self.snakes:
             snake.expend_energy(self.snake_energy_cost)
             if self.rng < self.strike_success_probability:
-                krat_index = self.select_krat()
-                krat = self.krats[krat_index]
+                krat = self.select_krat()
                 snake.consume(krat)
                 self.pop_krat() 
                 
@@ -65,14 +68,24 @@ class Cell(object):
 
 
 class Landscape(object):
-    def __init__(self,size_of_landscape,krat_energy_cost,snake_energy_cost,krat_energy_gain,cell_energy_pool, initial_snake_pop,initial_krat_pop,rng):
+    def __init__(self,size_of_landscape,krat_initial_energy,krat_energy_cost,krat_energy_gain,snake_initial_energy, snake_energy_cost,snake_strike_success_probability,cell_energy_pool, initial_snake_pop,initial_krat_pop,rng):
         #landscape is a container for cells and governs movements of critters in cells, and large sclae climate parameters.
+        #boskilla 1 x 0.8km 80,000 m^2
+        #bush (Larrea tridentata) under canopy of creosote up to 10m in diameter Square assumption (10x10m) = 100m^2 circle assumption pi*5^2 = 78.53m^2
+        #grass cells (Hilaria rigida) 1 m^2 clump +0.5 m^2 radius
+        #grass under or within 50cm of grass
+        #bush cover 1.9% (1520m^2), grass 24.1%(19,280m^2), open 74% (59000^2)
         self.cells = []
         self.size_of_landscape = size_of_landscape
+        self.initial_snake_pop = initial_snake_pop
+        self.initial_krat_pop = initial_krat_pop
+        self.krat_initial_energy = krat_initial_energy
+        self.snake_initial_energy = snake_initial_energy
         self.krat_energy_cost = krat_energy_cost
         self.snake_energy_cost = snake_energy_cost
         self.krat_energy_gain = krat_energy_gain
         self.cell_energy_pool = cell_energy_pool
+        self.snake_strike_success_probability = snake_strike_success_probability
         self.rng = rng
         self.gen_cell_list()
 
@@ -83,10 +96,21 @@ class Landscape(object):
     def add_cell(self,cell):
         self.cells.append(cell)
 
+    def add_snakes(self,cell):
+        for i in range(self.initial_snake_pop):
+            snake = Snake(energy_counter = self.snake_initial_energy,strike_success_probability = self.snake_strike_success_probability, rng = self.rng)
+            cell.add_snake(snake)
+
+    def add_krats(self,cell):
+        for i in range(self.initial_snake_pop):
+            krat = Krat(energy_counter = self.krat_initial_energy,home_cell = cell, rng = self.rng)
+            cell.add_krat(krat)
+
     def gen_cell_list(self):
         for i in range(size_of_landscape):
             habitat_type = self.assign_cell_type()
             cell = Cell(habitat_type,self.krat_energy_cost,self.snake_energy_cost,self.krat_energy_gain,self.cell_energy_pool, self.initial_snake_pop,self.initial_krat_pop,self.rng)
+            self.add_snakes(cell)
             self.add_cell(cell)
 
 

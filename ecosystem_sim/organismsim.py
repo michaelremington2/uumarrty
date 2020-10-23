ce410d1062862f57eaf54f3f0877574a3c1eb1e6
Jeet Sukumaran <jeetsukumaran@gmail.com>
2020-10-22 19:14:29 -0700

    Revision based on code review

--- ecosystem_sim/organismsim.py
+++ ecosystem_sim/organismsim.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 import math
-#doxygen 
+#doxygen
 #sphyinxdocumentation format
 class Organism(object):
     '''
     The base class that any tyoe of vertabrate organism can use to set up the object that represents the individual.
-    
+
     Args:
         sim -- the simulation object with base parameters such as a random number generator and a time parameter.
         energy counter -- The base energy stat that dictates the state of the organismand dictates feeding tendancies. This can be an int or a float.
@@ -43,13 +43,13 @@ class Organism(object):
 
     def expend_energy(self, energy_cost, energy_weight=1):
         '''
-        Sets the object to dead if there is not enough energy in the object.If alive, 
+        Sets the object to dead if there is not enough energy in the object.If alive,
         deducts the energy cost* the energy weight from the classes energy_counter attribute.
-        
+
         Args:
             energy_cost -- base constant to deduct from energy_counter (int/ float)
             energy_weight -- multiplier coefficent for the energy_cost factor.(optional set to 1 in no args passed.)
-        
+
         Raises:
             ValueError: if energy_cost is less than zero
         '''
@@ -82,8 +82,14 @@ class Organism(object):
             e = 0
         return e
 
-    def move_options(self): #calc move coordinates
-        self.move_coordinates = []
+    def calc_cell_destination_suitability(self, destination_cell):
+        return 1.0
+
+    def normalize_destination_cell_probabilities(self, destination_cell_probabilities):
+        return destination_cell_probabilities
+
+    def calc_destination_cell_probabilities(self): #calc move coordinates
+        destination_cell_probabilities = {}
         for x in range(-self.move_range,self.move_range+1):
             for y in range(-self.move_range,self.move_range+1):
                 #print('{},{}'.format(x,y))
@@ -91,58 +97,74 @@ class Organism(object):
                 column_coord = self.current_cell_id[1] + x
                 if row_coord >= 0 and row_coord <= self.row_boundary and column_coord >= 0 and column_coord <= self.column_boundary:
                     new_id = (row_coord,column_coord)
-                    self.move_coordinates.append(new_id)
+                    destination_cell = self.sim.landscape.select_cell(new_id)
+                    p = self.calc_cell_destination_suitability(destination_cell)
+                    destination_cell_probabilities[destination_cell] = p
         if len(self.move_coordinates) == 0:
             print(self.current_cell_id)
             print(self.row_boundary)
             print(self.column_boundary)
             raise ValueError('No move options')
+        self.normalize_destination_cell_probabilities(destination_cell_probabilities)
+        return destination_cell_probabilities
         #returns move_coordinates
 
-    def move_probability_base_vector(self):
-        base_probability_vector = []
-        for i in self.move_coordinates:
-            probability = 1/len(self.move_coordinates)
-            coord_prob = (i,probability)
-            base_probability_vector.append(coord_prob)
-        return base_probability_vector
+    # def move_probability_base_vector(self):
+    #     base_probability_vector = []
+    #     for i in self.move_coordinates:
+    #         probability = 1/len(self.move_coordinates)
+    #         coord_prob = (i,probability)
+    #         base_probability_vector.append(coord_prob)
+    #     return base_probability_vector
 
-    def gen_probability_vector(self,weight = 1):
-        base_probability_vector = self.move_probability_base_vector()
-        temp_probability_vector = []
-        for i in base_probability_vector:
-            cell_id = i[0]
-            probability = i[1]
-            if cell_id == self.current_cell_id:
-                enhanced_prob = probability*weight
-                temp_probability_vector.append(enhanced_prob)
-            else:
-                temp_probability_vector.append(probability)
-        return temp_probability_vector
+    # def gen_probability_vector(self,weight = 1):
+    #     base_probability_vector = self.move_probability_base_vector()
+    #     temp_probability_vector = []
+    #     for i in base_probability_vector:
+    #         cell_id = i[0]
+    #         probability = i[1]
+    #         if cell_id == self.current_cell_id:
+    #             enhanced_prob = probability*weight
+    #             temp_probability_vector.append(enhanced_prob)
+    #         else:
+    #             temp_probability_vector.append(probability)
+    #     return temp_probability_vector
 
-    def normalize_probability_vector(self,weight = 1):
-        temp_probability_vector = self.gen_probability_vector(weight = weight)
-        denom = sum(temp_probability_vector)
-        if denom != 1:
-            self.probability_vector = []
-            for i in temp_probability_vector:
-                probability = i/denom
-                self.probability_vector.append(probability)
-        else:
-            self.probability_vector = temp_probability_vector
+    # def normalize_probability_vector(self,weight = 1):
+    #     temp_probability_vector = self.gen_probability_vector(weight = weight)
+    #     denom = sum(temp_probability_vector)
+    #     if denom != 1:
+    #         self.probability_vector = []
+    #         for i in temp_probability_vector:
+    #             probability = i/denom
+    #             self.probability_vector.append(probability)
+    #     else:
+    #         self.probability_vector = temp_probability_vector
 
-    def new_cell(self,weight = 1):
-        self.move_options()
-        self.normalize_probability_vector(weight = weight)
+    def disperse_to_new_cell(self,weight = 1):
+        destination_cell_probabilities = self.calc_destination_cell_probabilities()
+        # pick a cell based on probability
+        # execute movement
+
+        # self.normalize_probability_vector(weight = weight)
         #print(self.move_coordinates)
         #print(self.probability_vector)
-        new_cell = self.rng.choices(self.move_coordinates,self.probability_vector,k=1)
-        return new_cell[0]
-
+        # new_cell = self.rng.choices(self.move_coordinates,self.probability_vector,k=1)
+        # return new_cell[0]
 
     def organism_movement(self, energy_dependence = True):
         '''Runs movement algorithm and returns the new cell id for the object to move to.'''
-        #roll dice to see if it moves
+
+        # if energy_dependence:
+        #     # do calc for energy dependence
+        #     if True: # if energy_deps results in movement:
+        #         return self.disperse_to_new_cell()
+        # elif self.rng.uniform() >= self.home_cell_relocation_probability:
+        #         return self.disperse_to_new_cell()
+        # return self.home_cell_id
+
+        #   # no dispersal
+        #   return self.home_cell_id
         #dictionary of probabilitys to cells basede on type
         if energy_dependence == True:
             e = self.homeostasis_state(x=1.5)
@@ -221,6 +243,12 @@ class Snake(Organism):
                                "hunting_hours": self.hunting_hours}
                 cell_incubation_list.append(snake_stats)
 
+    def calc_cell_destination_suitability(self, destination_cell):
+        return 1.0
+        # if cell is open:
+        #     return self.open_cell_preference_probability
+        # else:
+        #     return 1 - self.open_cell_preference_probability
 
 class Krat(Organism):
     def __init__(self,sim,initial_energy_counter,move_range,home_cell_id,foraging_rate,krat_max_litter_size,krat_litter_frequency,foraging_hours = None):

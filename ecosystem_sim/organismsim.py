ed7ab6321c8434dc22a3cf422004f4b8e948077e
michaelremington2 <michaelremington2@gmail.com>
2020-10-27 21:25:25 -0700

    Made progress on new movement algorithm but need to work on dictionary because right now it is not hashable

--- ecosystem_sim/organismsim.py
+++ ecosystem_sim/organismsim.py
@@ -16,13 +16,14 @@ class Organism(object):
         hungry -- if true the object will forage or consume at appropriate times if false the organisms energy state is to high to eat (boolean initial set to True).
         rng -- random number generator in the sim.
     '''
-    def __init__(self,sim,home_cell_id,initial_energy,energy_deviation, move_range):
+    def __init__(self,sim,home_cell,initial_energy,energy_deviation, move_range):
         self.sim = sim
         self.landscape =self.sim.landscape
-        self.initial_energy_counter = initial_energy_counter
+        self.initial_energy = initial_energy
         self.energy = initial_energy
-        self.max_energy = initial_energy_counter*energy_deviation #Assumption
-        self.hunger_level = initial_energy_counter*energy_deviation #Assumption
+        self.energy_deviation = energy_deviation
+        self.max_energy = initial_energy*self.energy_deviation #Assumption
+        self.hunger_level = initial_energy*self.energy_deviation #Assumption
         self.alive = True
         self.hungry = True
         self.predation_counter = 0
@@ -36,7 +37,7 @@ class Organism(object):
 
     def natural_death(self): #check_if_natural_death
         '''If the energy counter falls below zero, this function sets the alive attribute to false signifying it is dead.'''
-        if round(self.energy_counter) <= 0:
+        if round(self.energy) <= 0:
             self.alive = False
 
     def expend_energy(self, energy_cost, energy_weight=1):
@@ -55,30 +56,30 @@ class Organism(object):
         if self.alive == True:
             if energy_cost < 0:
                 raise ValueError('Costs should be positive integers')
-            self.energy_counter -= energy_cost*energy_weight
+            self.energy -= energy_cost*energy_weight
 
     def set_hungry(self):
         '''Controls hunger parameter true are false based on the energy counter and max_energy.'''
-        if self.energy_counter < self.hunger_level:
+        if self.energy < self.hunger_level:
             self.hungry =  True
-        elif self.energy_counter > self.max_energy:
+        elif self.energy > self.max_energy:
             self.hungry = False
 
 
-    def register_ predation_event(self):
+    def register_predation_event(self):
         self.predation_counter += 1
 
     def reset_predation_history(self):
         self.predation_counter = 0
 
-    def homeostasis_state(self, x):
-        e = (self.energy_counter - x**self.predation_counter)/self.initial_energy_counter #
+    def homeostasis_state(self):
+        e = (self.energy - math.exp(self.predation_counter))/self.initial_energy #
         if e < 0:
             e = 0
         return e
 
-    def calc_equal_cell_destination_suitability(self, cell, bush_microhabitat_weight=None, open_microhabitat_weight=None):
-        base_destination_probability = 1/len(self.landscape.cells)
+    def calc_cell_destination_suitability(self, cell, energy_weight=1, bush_microhabitat_weight=None, open_microhabitat_weight=None):
+        base_destination_probability = 1/len(self.landscape.cells)*energy_weight
         if bush_microhabitat_weight != None and cell.habitat_type == '[<MicrohabitatType.BUSH: 2>]':
             destination_probability = base_destination_probability*bush_microhabitat_weight
         elif open_microhabitat_weight != None and cell.MicrohabitatType.name == '[<MicrohabitatType.OPEN: 1>]':
@@ -88,81 +89,41 @@ class Organism(object):
         return destination_probability
 
     def normalize_destination_cell_probabilities(self, destination_cell_probabilities):
-        denom = sum(destination_cell_probabilities)
+        denom = sum(destination_cell_probabilities.values())
         if denom != 1:
-            normalized_probability_vector = {}
-            for i in destination_cell_probabilities:
-                probability = i/denom
-                 self.normalized_probability_vector.append(probability)
-         else:
-             self.probability_vector = temp_probability_vector
+            print(destination_cell_probabilities)
+            for i, prob in destination_cell_probabilities:
+                norm_probability = prob/denom
+                destination_cell_probabilities[i] == probability
         return destination_cell_probabilities
 
-    def calc_destination_cell_probabilities(self): #calc move coordinates
+    def calc_destination_cell_probabilities(self, energy_weight=1, bush_microhabitat_weight=None, open_microhabitat_weight=None): #calc move coordinates
         destination_cell_probabilities = {}
         for x in range(-self.move_range,self.move_range+1):
             for y in range(-self.move_range,self.move_range+1):
                 #print('{},{}'.format(x,y))
-                row_coord = self.current_cell_id[0] + y
-                column_coord = self.current_cell_id[1] + x
+                row_coord = self.current_cell.cell_id[0] + y
+                column_coord = self.current_cell.cell_id[1] + x
                 if row_coord >= 0 and row_coord <= self.row_boundary and column_coord >= 0 and column_coord <= self.column_boundary:
                     new_id = (row_coord,column_coord)
                     destination_cell = self.sim.landscape.select_cell(new_id)
-                    p = self.calc_cell_destination_suitability(destination_cell)
+                    p = self.calc_cell_destination_suitability(destination_cell,energy_weight=energy_weight, bush_microhabitat_weight=bush_microhabitat_weight, open_microhabitat_weight=open_microhabitat_weight)
                     destination_cell_probabilities[destination_cell] = p
-        if len(self.move_coordinates) == 0:
-            print(self.current_cell_id)
+        if len(destination_cell_probabilities) == 0:
+            print(self.current_cell.cell_id)
             print(self.row_boundary)
             print(self.column_boundary)
             raise ValueError('No move options')
-        self.normalize_destination_cell_probabilities(destination_cell_probabilities)
+        destination_cell_probabilities = self.normalize_destination_cell_probabilities(destination_cell_probabilities)
         return destination_cell_probabilities
-        #returns move_coordinates
 
-    # def move_probability_base_vector(self):
-    #     base_probability_vector = []
-    #     for i in self.move_coordinates:
-    #         probability = 1/len(self.move_coordinates)
-    #         coord_prob = (i,probability)
-    #         base_probability_vector.append(coord_prob)
-    #     return base_probability_vector
 
-    # def gen_probability_vector(self,weight = 1):
-    #     base_probability_vector = self.move_probability_base_vector()
-    #     temp_probability_vector = []
-    #     for i in base_probability_vector:
-    #         cell_id = i[0]
-    #         probability = i[1]
-    #         if cell_id == self.current_cell_id:
-    #             enhanced_prob = probability*weight
-    #             temp_probability_vector.append(enhanced_prob)
-    #         else:
-    #             temp_probability_vector.append(probability)
-    #     return temp_probability_vector
+    def pick_new_cell(self, energy_weight=1, bush_microhabitat_weight=None, open_microhabitat_weight=None):
+        destination_cell_probabilities = self.calc_destination_cell_probabilities( energy_weight=energy_weight, bush_microhabitat_weight=bush_microhabitat_weight, open_microhabitat_weight=open_microhabitat_weight)
+        new_cell = self.rng.choices(destination_cell_probabilities.values(),destination_cell_probabilities.keys(),k=1)
+        return new_cell[0]
 
-    # def normalize_probability_vector(self,weight = 1):
-    #     temp_probability_vector = self.gen_probability_vector(weight = weight)
-    #     denom = sum(temp_probability_vector)
-    #     if denom != 1:
-    #         self.probability_vector = []
-    #         for i in temp_probability_vector:
-    #             probability = i/denom
-    #             self.probability_vector.append(probability)
-    #     else:
-    #         self.probability_vector = temp_probability_vector
-
-    def disperse_to_new_cell(self,weight = 1):
-        destination_cell_probabilities = self.calc_destination_cell_probabilities()
-        # pick a cell based on probability
-        # execute movement
-
-        # self.normalize_probability_vector(weight = weight)
-        #print(self.move_coordinates)
-        #print(self.probability_vector)
-        # new_cell = self.rng.choices(self.move_coordinates,self.probability_vector,k=1)
-        # return new_cell[0]
-
-    def organism_movement(self, energy_dependence = True):
+    def organism_movement(self, bush_microhabitat_weight=None, open_microhabitat_weight=None, energy_dependence=True):
         '''Runs movement algorithm and returns the new cell id for the object to move to.'''
 
         # if energy_dependence:
@@ -176,34 +137,35 @@ class Organism(object):
         #   # no dispersal
         #   return self.home_cell_id
         #dictionary of probabilitys to cells basede on type
-        if energy_dependence == True:
+        if energy_dependence:
             e = self.homeostasis_state(x=1.5)
-            new_cell_id = self.new_cell(weight = e)
+            new_cell = self.pick_new_cell(energy_weight=e, bush_microhabitat_weight=bush_microhabitat_weight, open_microhabitat_weight=open_microhabitat_weight)
         else:
-            new_cell_id = self.new_cell()
-        if new_cell_id != self.current_cell_id:
+            new_cell = self.pick_new_cell(bush_microhabitat_weight=bush_microhabitat_weight, open_microhabitat_weight=open_microhabitat_weight)
+        if new_cell != self.current_cell:
                 self.reset_predation_history()
                 self.number_of_movements += 1
-        return new_cell_id
+        return new_cell
 
     def return_home(self):
-        return self.home_cell_id
+        return self.home_cell
 
 
 class Snake(Organism):
-    def __init__(self,sim, initial_energy_counter,move_range,home_cell_id,strike_success_probability,snake_max_litter_size,snake_litter_frequency,hunting_hours = None):
-        super().__init__(sim,home_cell_id,initial_energy_counter,move_range)
-        self.sim = sim
-        self.initial_energy_counter = initial_energy_counter
-        self.energy_counter = initial_energy_counter
+    def __init__(self,sim, initial_energy,energy_deviation,move_range,home_cell,strike_success_probability,snake_max_litter_size,snake_litter_frequency,hunting_hours = None):
+        super().__init__(sim,home_cell,initial_energy, energy_deviation, move_range)
+        self.sim = sim 
+        self.initial_energy= initial_energy
+        self.energy = initial_energy
         self._strike_success_probability = strike_success_probability
         self.snake_max_litter_size = snake_max_litter_size
         self.snake_litter_frequency = snake_litter_frequency*self.sim.time_step
-        self.home_cell_id = home_cell_id
+        self.home_cell = home_cell
         self.hunting = False
         self.hunting_hours = self.hunting_period_gen(hunting_hours)
         self.rng = self.sim.rng
         self.sex = self.rng.choice(['F','M'])
+        self.move_range = move_range
 
 
     @property
@@ -239,13 +201,13 @@ class Snake(Organism):
         if energy_gain < 0:
             raise ValueError('gains should be positive integers')
         else:
-            self.energy_counter += energy_gain
+            self.energy += energy_gain
 
     def reproduction(self,cell_incubation_list):
-        if self.rng.random() < self.snake_litter_frequency and self.sex == 'F' and self.sim.time_of_day == 6 and self.energy_counter >= self.initial_energy_counter:
+        if self.rng.random() < self.snake_litter_frequency and self.sex == 'F' and self.sim.time_of_day == 6 and self.energy >= self.initial_energy:
             litter_size = self.rng.randrange(0,self.snake_max_litter_size)
             for i in range(litter_size+1):
-                snake_stats = {"energy_counter": self.initial_energy_counter,
+                snake_stats = {"energy": self.initial_energy,
                                "move_range": self.move_range,
                                "strike_success_probability": self.strike_success_probability,
                                "snake_max_litter_size": self.snake_max_litter_size,
@@ -253,27 +215,21 @@ class Snake(Organism):
                                "hunting_hours": self.hunting_hours}
                 cell_incubation_list.append(snake_stats)
 
-    def calc_cell_destination_suitability(self, destination_cell):
-        return 1.0
-        # if cell is open:
-        #     return self.open_cell_preference_probability
-        # else:
-        #     return 1 - self.open_cell_preference_probability
-
 class Krat(Organism):
-    def __init__(self,sim,initial_energy_counter,move_range,home_cell_id,foraging_rate,krat_max_litter_size,krat_litter_frequency,foraging_hours = None):
-        super().__init__(sim,home_cell_id,initial_energy_counter,move_range)
+    def __init__(self,sim,initial_energy,energy_deviation,move_range,home_cell,foraging_rate,krat_max_litter_size,krat_litter_frequency,foraging_hours = None):
+        super().__init__(sim,home_cell,initial_energy,energy_deviation,move_range)
         self.sim = sim
-        self.initial_energy_counter = initial_energy_counter
-        self.energy_counter = initial_energy_counter
+        self.initial_energy = initial_energy
+        self.energy= initial_energy
         self.krat_max_litter_size = krat_max_litter_size
         self.krat_litter_frequency = krat_litter_frequency
-        self.home_cell_id = home_cell_id
+        self.home_cell = home_cell
         self.foraging = False
         self.foraging_hours = self.foraging_period_gen(foraging_hours)
         self.foraging_rate = foraging_rate
         self.rng = self.sim.rng
         self.sex = self.rng.choice(['F','M'])
+        self.move_range = move_range
 
     def foraging_period_gen(self,foraging_hours):
         if foraging_hours == None:
@@ -282,7 +238,7 @@ class Krat(Organism):
 
     def foraging_period(self):
         self.set_hungry()
-        if self.sim.time_of_day in self.foraging_hours and self.hungry == True:
+        if self.sim.time_of_day in self.foraging_hours and self.hungry:
             self.foraging = True
         else:
             self.foraging = False
@@ -300,7 +256,7 @@ class Krat(Organism):
         return energy_gain
 
     def energy_gain(self,cell_energy_pool):
-        if self.alive == True:
+        if self.alive:
             if self.foraging_rate*cell_energy_pool % 1 != 0:
                 energy_gain = self.energy_calculator(cell_energy_pool)
             else:
@@ -308,16 +264,16 @@ class Krat(Organism):
             return energy_gain
 
     def forage(self,energy_gain):
-        if self.alive == True:
-            self.energy_counter += energy_gain
+        if self.alive:
+            self.energy += energy_gain
 
     def reproduction(self,cell_incubation_list):
         random_prob = self.rng.random()
-        if (random_prob < self.krat_litter_frequency) and self.sex == 'F' and self.energy_counter >= self.initial_energy_counter:
+        if (random_prob < self.krat_litter_frequency) and self.sex == 'F' and self.energy >= self.initial_energy:
             #print('prob {}, krat_freq {}, sex {}'.format(random_prob,self.krat_litter_frequency,self.sex))
             litter_size = self.rng.randrange(0,self.krat_max_litter_size)
             for i in range(litter_size+1):
-                krat_stats = {"energy_counter": self.initial_energy_counter,
+                krat_stats = {"energy": self.initial_energy,
                               "move_range": self.move_range,
                               "foraging_rate": self.foraging_rate,
                               "krat_max_litter_size": self.krat_max_litter_size,

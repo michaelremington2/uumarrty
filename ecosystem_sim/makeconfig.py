da7b7585909119f7a47b01be6f411dfc6f29c015
michaelremington2 <michaelremington2@gmail.com>
2020-11-26 18:32:13 -0800

    Made small changes to config file

--- ecosystem_sim/makeconfig.py
+++ ecosystem_sim/makeconfig.py
@@ -7,21 +7,23 @@ data['sim'] = []
 data['sim'].append({
     "days_of_sim": 3650,
     "time_step":1,
-    "energy_dependence_movement":False,
     "landscape_size_x": 500,
     "landscape_size_y": 500,
     "microhabitat_open_bush_proportions": [0.5,0.5],
     "initial_snake_pop": 8,
     "initial_krat_pop": 100,
-    "initial_owl_pop": 2,
-    "snake_strike_success_probability_bush": 0.032,
-    "snake_strike_success_probability_open": 0.009,
-    "krat_energy_gain_bush": 12,
-    "krat_energy_gain_open": 12,
+    "initial_owl_pop": 1,
+    "snake_strike_success_probability_bush": 0.032, #from bouskila
+    "snake_strike_success_probability_open": 0.009, #from bouskila
+    "krat_energy_gain_bush": 12, #from bouskila
+    "krat_energy_gain_open": 12, #from bouskila
+    "snake_energy_gain": (1600/5), #krat total reproduction death cost divided by 5 because thats about how many kids a krat has on average in its life. This number prolly needs some work.
+    "krat_energy_cost":,
+    "snake_energy_cost":,
     "krat_move_range":1,
     "snake_move_range":4,
     "owl_move_range":6,
-    "owl_catch_success":0.02,
+    "owl_catch_success":0.02, #from bouskila
     "krat_open_preference_weight":1,
     "krat_bush_preference_weight":1,
     "snake_open_preference_weight":1,

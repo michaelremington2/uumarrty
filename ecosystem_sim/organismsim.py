eef6c80d7b50d65337f92be862e9ef1afb6e22e3
michaelremington2 <michaelremington2@gmail.com>
2021-02-26 20:46:37 -0800

    Added lots of comments to algorithms and fixed a bug in the movement alg. Added a new chart for post sim analysis.

--- ecosystem_sim/organismsim.py
+++ ecosystem_sim/organismsim.py
@@ -5,17 +5,26 @@ import statistics as stats
 #sphyinxdocumentation format
 class Organism(object):
     '''
-    The base class that any tyoe of vertabrate organism can use to set up the object that represents the individual.
+    The base class that any type of vertabrate organism can use to set up the object that represents the individual.
 
     Args:
-        sim -- the simulation object with base parameters such as a random number generator and a time parameter.
-        energy counter -- The base energy stat that dictates the state of the organismand dictates feeding tendancies. This can be an int or a float.
+        sim -- the simulation object with base parameters such as a random number generator and a time parameter. (sim object)
+        move_range -- the distance the organism can move from it's current cell. Orgs can move (N,E,W,S,NW,NE,SE,SW). (default 1)
+        home_cell -- cell the organism is born in or has a nest. (tuple of x,y)
+        move_preference -- an algorithm for weighting cells that previously provided the organism with increased payoff. This currently isn't working so set to false.
+        open_preference_weight -- a weight that either increases or decreases prference to the open habitat. (default 1)
+        bush_preference_weight -- a weight that either increases or decreases prference to the bush habitat. (default 1)
+        memory_length_cycles -- this is the number of cycles the organism will have memory of its payoff values for. This goes into the move preference algorithm that isn't currently working. (bool)
 
     Attributes:
-        max_energy -- initial energy counter.
-        alive -- if true the object is alive, if false the object is dead (boolean initial set to True).
-        hungry -- if true the object will forage or consume at appropriate times if false the organisms energy state is to high to eat (boolean initial set to True).
-        rng -- random number generator in the sim.
+        alive -- if true the object is alive, if false the object is dead (bool).
+        landscape -- the landscape object the organism opperates on. (class object)
+        energy_score -- a rolling sum of the payoffs the organism is accruing throughout its life cycle. (int)
+        column_boundary -- this is the boundary of the landscape in the x direction. (int)
+        row_boundary -- this is the boundary of the landscape in the y direction. (int)
+        number_of_movements -- a rolling count of the number of movements an organism makes when it moves outside of its current cell. (int)
+        rng -- random number generator in the sim. (random object)
+        microhabitat_energy_log -- 
     '''
     def __init__(self,sim,home_cell=None, move_range=1,move_preference=False, open_preference_weight=1, bush_preference_weight=1,memory_length_cycles=0):
         self.sim = sim
@@ -38,18 +47,20 @@ class Organism(object):
             self.microhabitat_energy_log = {'BUSH': [None]*self.memory_length_cycles, 'OPEN':[None]*self.memory_length_cycles}
             self.bush_preference_log_counter = 0
             self.open_preference_log_counter = 0
-        self.data_log = []
 
     def __hash__(self):
         return id(self)
 
     def register_predation_event(self):
+        '''Adds one predation event to the rolling count if the predation event is unsuccessful.'''
         self.predation_counter += 1
 
     def reset_predation_history(self):
+        '''resets the attribute predation_counter to zero.'''
         self.predation_counter = 0
 
     def log_microhabitat_energy_delta_preference(self,microhabitat_type,energy_delta):
+        '''logs the payoff for  particular habitat type and stores it into the appropriate microhabitat preference log. This is used in the energy preference algorithm and currently doesn't work.'''
         if microhabitat_type == 'BUSH':
             self.microhabitat_energy_log['BUSH'][self.bush_preference_log_counter] = energy_delta
             if self.bush_preference_log_counter == self.memory_length_cycles-1:
@@ -66,6 +77,7 @@ class Organism(object):
             raise ValueError('Enter Valid Microhabitat Name')
 
     def normalize_energy_delta_preferences(self,energy_delta_set):
+        '''Apart of the move preference algorithm. this normalizes the set of energy deltas and returns an interger of the preference weight.'''
         min_energy = min(energy_delta_set)
         max_energy = max(energy_delta_set)
         if (max_energy-min_energy) == 0:
@@ -79,12 +91,14 @@ class Organism(object):
         return norm_weight
 
     def gen_new_microhabitat_preferences(self):
+        '''Apart of the move preference algorithm. Redefines microhabitat preferences once the energy logs are fully populated. This alg is not currently working.'''
         if None not in self.microhabitat_energy_log['BUSH']:
             self.bush_preference_weight= self.normalize_energy_delta_preferences(self.microhabitat_energy_log['BUSH'])
         if None not in self.microhabitat_energy_log['OPEN']:
             self.open_preference_weight= self.normalize_energy_delta_preferences(self.microhabitat_energy_log['OPEN'])
 
     def calc_cell_destination_suitability(self, cell,number_of_move_options):
+        '''Calculates the probability an organism will move to a particular cell. The base probabilty is the number cells the organism can move to and it is multiplied by the organisms affinity to the microhabitat.'''
         if self.move_preference:
             self.gen_new_microhabitat_preferences()
         if number_of_move_options <= 0:
@@ -100,6 +114,7 @@ class Organism(object):
         return destination_probability
 
     def normalize_destination_cell_probabilities(self, destination_cell_probabilities):
+        '''Returns a normalized probability list of all the potential destination cells.'''
         denom = sum(destination_cell_probabilities.values())
         #print('denom {}'.format(denom))
         for i, prob in destination_cell_probabilities.items():
@@ -116,6 +131,7 @@ class Organism(object):
         return destination_cell_probabilities
 
     def calibrate_move_distance_with_boundaries(self,current_coord_x,current_coord_y, move_distance,x_boundary,y_boundary):
+        '''Function to make sure organsim only moves within boundaries. Only used in movement algorithm.'''
         if (current_coord_x + move_distance) > x_boundary:
             move_dist_x_right = x_boundary - current_coord_x
         else:
@@ -139,6 +155,7 @@ class Organism(object):
 
 
     def calc_destination_cell_probabilities(self, bush_preference_weight, open_preference_weight): #calc move coordinates
+        '''This is the main function in the movement algorithm that generates a noralimized destination cell probability vector.'''
         destination_cell_probabilities = {}
         move_options = []
         calibrated_move_dists = self.calibrate_move_distance_with_boundaries(current_coord_x=self.current_cell.cell_id[1],
@@ -168,9 +185,10 @@ class Organism(object):
             print(self.column_boundary)
             raise ValueError('No move options')
         norm_destination_cell_probabilities = self.normalize_destination_cell_probabilities(destination_cell_probabilities)
-        return destination_cell_probabilities
+        return norm_destination_cell_probabilities
 
     def pick_new_cell(self, bush_preference_weight=1, open_preference_weight=1):
+        '''Picks a destination cell from the probability vector.'''
         destination_cell_probabilities = self.calc_destination_cell_probabilities( bush_preference_weight=bush_preference_weight, open_preference_weight=open_preference_weight)
         new_cell = self.rng.choices(list(destination_cell_probabilities.keys()),list(destination_cell_probabilities.values()),k=1)
         return new_cell[0]
@@ -183,23 +201,13 @@ class Organism(object):
         return new_cell
 
     def return_home(self):
+        '''Returns the home cell to be used in an algorithm for movment if the org needs to return to nest.'''
         return self.home_cell
 
     def populate_microhabitat_energy_log(self,microhabitat_type,delta_energy_score):
+        '''Used in preference algorithm. Populates the dictionary of the microhabitat preference log.'''
         label = microhabitat_type
         self.microhabitat_energy_log[label] += delta_energy_score
-
-    def populate_data_analysis_log(self,org_id,microhabitat_type,delta_energy_score,energy_score,number_of_other_org,number_of_owls):
-        #print('pbush {}, popen {}'.format(self.bush_preference_weight,self.open_preference_weight))
-        data = [org_id,self.sim.cycle,microhabitat_type,delta_energy_score,energy_score,number_of_other_org,number_of_owls,self.number_of_movements]
-        #print(data)
-        if self.sim.cycle % 100==0:
-            print('Bush Preference {}, Open Preference {}'.format(self.bush_preference_weight,self.open_preference_weight))
-            print('bush')
-            print(self.microhabitat_energy_log['BUSH'])
-            print('open')
-            print(self.microhabitat_energy_log['OPEN'])
-        self.data_log.append(data)
 
 
 class Snake(Organism):

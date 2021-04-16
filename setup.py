7d40d426dbe979a7480fedd6fc36869fa0c7129b
michaelremington2 <michaelremington2@gmail.com>
2021-04-15 18:11:06 -0700

    added a manifest script and updated the setup.py script to match the tutorial.

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 #! /usr/bin/env python
 from setuptools import setup, find_packages
 
+with open("README.txt", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='Krattle',
     version='0.1',
-    packages=['Krattle',],
+    package_dir={"src": "krattle",
+                "": "bin"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
     license='bsd',
     classifiers=[
     "Intended Audience :: Science/Research",
@@ -12,20 +18,14 @@ setup(
     "Natural Language :: English",
     "Operating System :: OS Independent",
     "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.1",
-    "Programming Language :: Python :: 3.2",
-    "Programming Language :: Python :: 3.3",
-    "Programming Language :: Python :: 3.4",
-    "Programming Language :: Python :: 3.5",
-    "Programming Language :: Python :: 3.6",
-    'Programming Language :: Python :: 3.8.5'
     ],
     scripts=[
-        "ecosystem_sim/sim.py",
-        "ecosystem_sim/organismsim.py",
+        "src/krattle/sim.py",
+        "src/krattle/organismsim.py",
+        "bin/run_krattle.py",
         ],
     url='https://github.com/michaelremington2/Krattle',
     author='Michael Remington and Jeet Sukumaran',
     author_email='micahelremington2@gmail.com and jsukumaran@sdsu.edu',
-    long_description=open('README.txt').read(),
+    long_description=long_description,
 )

47f83763d6eb70ad3da363ef9a8bfa4a7d140dea
Michael Remington <michaelremington2@gmail.com>
2021-06-28 19:04:20 -0700

    setup and tear down methods werent actually class methods.

--- tests/test_sim_config.py
+++ tests/test_sim_config.py
@@ -6,7 +6,7 @@ from uumarrty.sim import Sim
 
 class TestSimConfig(unittest.TestCase):
     
-    @classmethod
+
     def setUp(self):
         print('setup and initalizing null sim')
         self.data_file = 'Data/null_exp.txt'
@@ -14,7 +14,7 @@ class TestSimConfig(unittest.TestCase):
             self.config_d = json.load(f)
         #self.null_sim = Sim(initial_conditions_file_path = 'Data/null_exp.txt', krat_tsv_output_file_path = 'Data/null_exp_krat_energy.tsv', snake_tsv_output_file_path = 'Data/null_exp_snake_energy.tsv',rng=None)
 
-    @classmethod
+
     def tearDown(self):
         print('Tearing Down')
 
